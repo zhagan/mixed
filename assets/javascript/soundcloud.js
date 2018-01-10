95b5d1692476271458c28ea0f60df25064a33a8d
Amy Lyne <amy.lyne92@gmail.com>
2018-01-09 20:26:26 -0700

    bolded mix

--- assets/javascript/soundcloud.js
+++ assets/javascript/soundcloud.js
@@ -390,7 +390,7 @@ function mixed(){
 
          });
          widget.bind(SC.Widget.Events.PAUSE, function() {
-           player.hide();
+           // player.hide();
 
        });
            widget.bind(SC.Widget.Events.FINISH, function() {
